6bef49b984d61b675634e620e68967a00c639b13
xukai0324 <30818557+xukai0324@users.noreply.github.com>
2017-08-09 10:54:54 +0800

    xukai

---
+++ source/note/web/src/main/java/com/james/note/web/WebApplication.java
@@ -0,0 +1,14 @@
+package com.james.note.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Admin on 2017/8/9.
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
---
+++ source/note/web/src/main/java/com/james/note/web/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.james.note.web.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Admin on 2017/8/9.
+ */
+@RestController
+public class HelloController {
+    @Value("${spring.foo}")
+    String name;
+
+    @RequestMapping(value = "hello")
+    public String sayHello() {
+        return "hello : " + name;
+    }
+}
